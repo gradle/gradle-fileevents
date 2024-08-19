1d420dee2d9ab1dd1cfdf533a9db2524bf1cafd1
Lóránt Pintér <lorant.pinter@gmail.com>
2024-08-20 00:20:40 +0200

    Temporarily disable publishing to ge.gradle.org to make GitHub builds work

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("com.gradle.develocity").version("3.17.5")
-    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.1")
+//    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.1")
 }
 
 rootProject.name = "file-events"
