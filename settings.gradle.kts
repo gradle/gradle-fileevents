83cd7ba677f7db4020c96621a6b8a8c55e709db0
Lóránt Pintér <lorant.pinter@gmail.com>
2024-08-21 21:09:10 +0200

    Update Build Scan plugin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.gradle.develocity").version("3.17.5")
+    id("com.gradle.develocity").version("3.18")
 //    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.1")
 }
 
