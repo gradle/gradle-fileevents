f047e83d5d5e9467b05551fc9688e10a87754f58
Lóránt Pintér <lorant@gradle.com>
2024-08-26 21:41:16 +0200

    Resolve Git version from root of build
    This helps if included from another build.

--- build-logic/src/main/kotlin/gradlebuild.git-version.gradle.kts
+++ build-logic/src/main/kotlin/gradlebuild.git-version.gradle.kts
@@ -1,17 +1,18 @@
 import gradlebuild.GenerateVersions
-import org.gradle.api.provider.ValueSource
-import org.gradle.api.provider.ValueSourceParameters
-import org.gradle.process.ExecOperations
 import java.io.ByteArrayOutputStream
-import javax.inject.Inject
 
 abstract class GitVersionValueSource @Inject constructor(val exec: ExecOperations) :
-    ValueSource<String, ValueSourceParameters.None> {
+    ValueSource<String, GitVersionValueSource.Params> {
+        interface Params : ValueSourceParameters {
+            val rootDir: DirectoryProperty
+        }
+
     override fun obtain(): String? {
         return ByteArrayOutputStream().use { outputStream ->
             exec.exec {
                 commandLine("git", "describe", "--tags")
                 standardOutput = outputStream
+                workingDir = parameters.rootDir.get().asFile
             }
             outputStream.toString().trim()
         }
@@ -25,7 +26,9 @@ interface GitVersionExtension {
 }
 
 val extension = project.extensions.create<GitVersionExtension>("git")
-extension.version = providers.of(GitVersionValueSource::class.java) {}
+extension.version = providers.of(GitVersionValueSource::class.java) {
+    parameters.rootDir = project.rootDir
+}
 
 val generateVersionFile by tasks.registering(GenerateVersions::class) {
     version = extension.version
