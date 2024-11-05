8b8678fe1ce98caa5dd13b0347a6db6941ab5d46
Christoph Obexer <cobexer@gradle.com>
2024-11-05 14:39:45 +0100

    Update Develocity Plugin(s)
    Signed-off-by: Christoph Obexer <cobexer@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,8 +11,8 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("com.gradle.develocity").version("3.18")
-    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.1")
+    id("com.gradle.develocity").version("3.18.1")
+    id("io.github.gradle.gradle-enterprise-conventions-plugin").version("0.10.2")
 }
 
 develocity {
