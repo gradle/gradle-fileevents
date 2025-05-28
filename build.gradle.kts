6f4d4c29400c0dd8a145b83dd23f1f75c758fd19
Anže Sodja <asodja@gradle.com>
2025-05-28 11:01:06 +0200

    Use latest stable Zig version 0.14.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ sourceSets {
 }
 
 zig {
-    zigVersion = "0.15.0-dev.286+ddcf6fcdf"
+    zigVersion = "0.14.1"
     outputDir = layout.buildDirectory.dir("zig")
     targets {
         create("x86_64-linux-gnu")
